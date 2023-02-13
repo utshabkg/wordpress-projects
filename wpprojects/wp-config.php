a67208ee2eac6000911d6723d21e8bb7cb45255d
utshabkg <ranjanyouth@gmail.com>
2023-02-13 16:01:56 +0600

    screenshot. debug=true added

--- wpprojects/wp-config.php
+++ wpprojects/wp-config.php
@@ -107,7 +107,8 @@ $table_prefix = getenv_docker('WORDPRESS_TABLE_PREFIX', 'wp_');
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define('WP_DEBUG', !!getenv_docker('WORDPRESS_DEBUG', ''));
+// define('WP_DEBUG', !!getenv_docker('WORDPRESS_DEBUG', ''));
+define( 'WP_DEBUG', true );
 define('FS_METHOD', 'direct');
 
 
